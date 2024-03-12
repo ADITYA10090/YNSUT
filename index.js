78ccda0fe760ae8b87f4981f32b7de6a922e5805
ADITYA10090 <124758596+ADITYA10090@users.noreply.github.com>
2024-03-12 14:39:44 +0530

    Update index.js

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ btn?.addEventListener("click", async ()=>{
         await fetch("http://localhost:5000/attendance").then(response=>response.json()).then(data=>console.log(data));
         // const data = await response.json();
         // console.log(data);
+//         const xhr = new XMLHttpRequest();
+//         xhr.open('GET', 'https://www.googleapis.com/discovery/v1/apis');
+//         xhr.onload = () => console.log(JSON.parse(xhr.responseText));
+//         xhr.send();
     } catch (error) {
         console.log(error);
     }
